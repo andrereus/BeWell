1e26227863ba04f94b64568095e0f8048a449a33
Andre Reus <andrereus@users.noreply.github.com>
2023-06-02 15:47:54 +0200

    Add extension

---
+++ BeWell/Libs/Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Extensions.swift
+//  BeWell
+//
+//  Created by Student on 02.06.23.
+//
+
+import SwiftUI
+
+extension Binding {
+    func onChange(_ handler: @escaping (Value) -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler(newValue)
+            }
+        )
+    }
+}
